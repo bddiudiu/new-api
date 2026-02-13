0609d3060b110307882abeb41dcac0d8671c1c1e
Adam.Wang <bddiudiu@gmail.com>
2026-02-13 17:06:30 +0800

    feat: Create a user API to synchronously return the user ID, Groups and simultaneously generate a default token according to the configuration.

--- controller/user.go
+++ controller/user.go
@@ -793,9 +793,39 @@ func CreateUser(c *gin.Context) {
 		return
 	}
 
+	// 生成默认令牌
+	if constant.GenerateDefaultToken {
+		key, err := common.GenerateKey()
+		if err != nil {
+			common.ApiErrorI18n(c, i18n.MsgUserDefaultTokenFailed)
+			common.SysLog("failed to generate token key: " + err.Error())
+			return
+		}
+		// 生成默认令牌
+		token := model.Token{
+			UserId:             cleanUser.Id, // 使用插入后的用户ID
+			Name:               cleanUser.Username + "的初始令牌",
+			Key:                key,
+			CreatedTime:        common.GetTimestamp(),
+			AccessedTime:       common.GetTimestamp(),
+			ExpiredTime:        -1,     // 永不过期
+			RemainQuota:        500000, // 示例额度
+			UnlimitedQuota:     true,
+			ModelLimitsEnabled: false,
+		}
+		if setting.DefaultUseAutoGroup {
+			token.Group = "auto"
+		}
+		if err := token.Insert(); err != nil {
+			common.ApiErrorI18n(c, i18n.MsgCreateDefaultTokenErr)
+			return
+		}
+	}
+
 	c.JSON(http.StatusOK, gin.H{
 		"success": true,
 		"message": "",
+		"userId":  cleanUser.Id,
 	})
 	return
 }
